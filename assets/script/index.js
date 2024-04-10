71d8ffd6bb12d47fbbfc1cb397e14660dc6a294b
Henry <hbreukelman20@gmail.com>
2024-04-10 14:11:44 -0500

    added selectors and listeners

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,21 +1,25 @@
 'use strict';
 
-
-
-
 /*
   selectors
 */
 
-
+const findButton = document.querySelector('.find-button');
+const map = document.querySelector('.map-box');
+let longitude = 0;
+let latitude = 0;
 
 /*
   functions
 */
 
+function loadMap() {
 
+}
 
 /*
   eventlisteners
 */
 
+findButton.addEventListener('click', loadMap);
+window.addEventListener('load', loadMap);
